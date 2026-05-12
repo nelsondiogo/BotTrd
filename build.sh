c0957d6ee9fb1060e8ebff4a229537d353d80c49
nelsondiogo <jose84nelson@gmail.com>
2026-05-12 15:09:27 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Instalação rápida sem compilação
-pip install --no-cache-dir --upgrade pip
-pip install --no-cache-dir flask ccxt pandas numpy gunicorn aiohttp aiofiles
-
-# Executa
-exec gunicorn app:app --bind 0.0.0.0:$PORT --workers 1 --threads 4 --timeout 120
